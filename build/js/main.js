54da5155ce365f9a92b136ecb16fa181fbdf6611
nikitashvetsov21 <62443471+nikitashvetsov21@users.noreply.github.com>
2021-04-12 03:27:56 -0700

    add merch

--- build/js/main.js
+++ build/js/main.js
@@ -1,9 +1,10 @@
 function openBlock() {
     const someId = document.getElementById("someId");
     const otherId = document.getElementById("other");
-
+    const shopToggle = document.getElementById("shopToggle");
     someId.classList.toggle("active");
     otherId.classList.toggle("active");
+    shopToggle.classList.toggle("active");
 }
 
 // $(document).ready(function () {
