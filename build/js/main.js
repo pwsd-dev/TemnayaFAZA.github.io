588357b4f19c3193a537dd3c503a7f54e5b3437e
nikitashvetsov21 <62443471+nikitashvetsov21@users.noreply.github.com>
2021-04-14 02:55:18 -0700

    shop article in process

--- build/js/main.js
+++ build/js/main.js
@@ -11,7 +11,7 @@ function openBlock() {
 $(function () {
     $('.nav li').on('click', function () {
         $(this).addClass('active').siblings().removeClass('active');
-        $('#'+$(this).data('id')).addClass('selected').siblings().removeClass('selected');
+        $('#' + $(this).data('id')).addClass('selected').siblings().removeClass('selected');
     });
 });
 
@@ -31,4 +31,48 @@ $(document).ready(function () {
     //     $("#burgerBtn").click(function () {
     //         $("#other").fadeOut();
     //     });
+});
+
+
+$(document).ready(function () {
+    $('.shop-article-slider').slick({
+        // arrows: false,
+        // // dots: true,
+        // slidesToShow: 1,
+        // // autoplay: true,
+        // // infinite: true,
+        // // speed: 1000,
+        // adaptiveHeight: false,
+        // pauseOnFocus: true,
+        // pauseOnHover: true,
+        // pauseOnDotsHover: true,
+        // // easing: 'ease',
+        // draggale: true,
+        // swipe: true,
+        // touchTreshold: 20,
+        // touchMove: false,
+        // fade: true,
+        // // prevArrow: document.querySelector('#prevArrow'),
+        // // nextArrow: document.querySelector('#nextArrow'),
+        // asNavFor: '.shop-article-nav'
+        draggable: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.shop-article-nav'
+    });
+
+});
+
+$('.shop-article-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.shop-article-slider',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+    variableWidth: true,
+    vertical: true
+
 });
