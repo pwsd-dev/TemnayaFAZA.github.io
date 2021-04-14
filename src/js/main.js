54a97c9b83c76a53d2c42c2aaa71b5f37b0aab58
nikitashvetsov21 <62443471+nikitashvetsov21@users.noreply.github.com>
2021-04-14 06:19:21 -0700

    add shop-article

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,13 @@ $(function () {
     });
 });
 
+$(function () {
+    $('.nav-shop-article li').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+        $('#' + $(this).data('id')).addClass('selected').siblings().removeClass('selected');
+    });
+});
+
 $(document).ready(function () {
     $("#burgerBtn").click(function () {
         if ($("#other").hasClass('active')) {
@@ -73,6 +80,39 @@ $('.shop-article-nav').slick({
     centerMode: true,
     focusOnSelect: true,
     variableWidth: true,
+    draggable: false,
     vertical: true
 
 });
+
+const selectSingle = document.querySelector('.__select');
+const selectSingle_title = selectSingle.querySelector('.__select__title');
+const selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
+
+// Toggle menu
+selectSingle_title.addEventListener('click', () => {
+    if ('active' === selectSingle.getAttribute('data-state')) {
+        selectSingle.setAttribute('data-state', '');
+    } else {
+        selectSingle.setAttribute('data-state', 'active');
+    }
+});
+
+// Close when click to option
+for (let i = 0; i < selectSingle_labels.length; i++) {
+    selectSingle_labels[i].addEventListener('click', (evt) => {
+        selectSingle_title.textContent = evt.target.textContent;
+        selectSingle.setAttribute('data-state', '');
+    });
+}
+
+// Reset title
+const reset = document.querySelector('.reset');
+reset.addEventListener('click', () => {
+    selectSingle_title.textContent = selectSingle_title.getAttribute('data-default');
+});
+
+
+
+
+
